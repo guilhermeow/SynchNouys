1702c53a956872591294b10a1688a1158ef37710
sicko <45300362+sickog0d@users.noreply.github.com>
2021-03-02 19:51:53 -0300

    Add files via upload

---
+++ utils/admin/array.js
@@ -0,0 +1,8 @@
+module.exports = [
+    "admin.php", "admin.html", "admin", "administrator/", "webadmin", "admin/admin.html",
+    "adminLogin/", "administratorLogin/", "adm/", "siteadmin/login.php", "admin/admin.php",
+    "admin-login.php", "admin-login.html", "adm.php", "adm.html", "admin/admin-login.html",
+    "login/admin.php", "login/admin.html", "login/admin/", "admin/admin-login.php", "administrator.html",
+    "administrator.php", "adm_login.php", "administrator/login.php", "administrator/login.html",
+    "admin/login.php", "admin/login.html", ,
+];
---
+++ utils/colors.js
@@ -0,0 +1,5 @@
+module.exports = {
+    red: "\033[0;31m",
+    green: "\033[0;32m",
+    reset: "\x1b[0m",
+};
